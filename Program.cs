6f7f311a57f498721045ad08928e220eb92cb5a3
Jose Ramirez <joseramirez@Joses-MacBook-Pro.local>
2022-06-14 08:41:51 -0600

    Rectangle Area

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿int length;
+Console.WriteLine("Enter the length of the rectangle");
+length = int.Parse(Console.ReadLine());
+
+int width;
+Console.WriteLine("Enter the width of the rectangle");
+width = int.Parse(Console.ReadLine());
+
+Console.WriteLine("The area of the rectangle is:");
+Console.WriteLine(length * width);
